f46305434d7ce0059df07077ff1644075062d720
Luis Lacher <Luis.lacher@telekom.de>
2024-11-15 15:32:38 +0100

    Add multizoom

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,8 @@
+import { isCallExpression } from "../../../node_modules/typescript/lib/typescript";
 import { normalColorCalculation, getModuloColorCalculation, getColorFnWithBackground } from "./colorFn";
 import { initialMandelbrotParams } from "./data";
 import { createCanvas, drawMandelbrot, Mandelbrot, MandelbrotParams, zoom } from "./mandelbrot";
-import { enterFullscreen } from "./utils";
+import { delay, doXTimesEveryYms, enterFullscreen } from "./utils";
 
 const canvas = createCanvas(1000, 1000)
 const red = document.getElementById("red") as HTMLInputElement
@@ -12,15 +13,26 @@ const backgroundGreen = document.getElementById("backgroundGreen") as HTMLInputE
 const backgroundBlue= document.getElementById("backgroundBlue") as HTMLInputElement
 const maxIterations = document.getElementById("MaxInterations") as HTMLInputElement
 const zoomFactor = document.getElementById("zoomFactor") as HTMLInputElement
+const zoomSteps = document.getElementById("zoomSteps") as HTMLInputElement
+const zoomDelay = document.getElementById("zoomDelay") as HTMLInputElement
 const activateModuloColor = document.getElementById("activatModuloColor") as HTMLInputElement
 
 let currentZoomFactor = 1.2
+let currentZoomSteps = 1
+let currentZoomDelay = 1000
 let backgroundColor = {r: +backgroundRed.value, g:+backgroundGreen.value, b:+backgroundBlue.value}
 let currentColorFunction = getColorFnWithBackground(normalColorCalculation)
 let currentMandelbrot:Mandelbrot = drawMandelbrot({
   ...initialMandelbrotParams,
   getColorFn: currentColorFunction(backgroundColor)
 }, canvas)
+let waitingMandelBrot = new Promise<boolean>((r)=>{r(true)})
+
+const pipeline =async (callback:any) =>{
+  if(await waitingMandelBrot.then()){
+    waitingMandelBrot = new Promise<boolean> ((resolve)=>{callback(); resolve(true)})
+  }
+}
 
 function updateHtml(newMandelbrot: Mandelbrot){
   currentMandelbrot = newMandelbrot
@@ -31,7 +43,7 @@ function updateHtml(newMandelbrot: Mandelbrot){
 function changeMandelbrotParams(change: Record<string,any>| null){
   if(!currentMandelbrot) return
   const newParams: MandelbrotParams = {...currentMandelbrot.parameter,...change}
-  updateHtml(drawMandelbrot(newParams,canvas))
+  pipeline(()=>updateHtml(drawMandelbrot(newParams,canvas)))
 }
 
 updateHtml(currentMandelbrot)
@@ -41,15 +53,15 @@ canvas.mandelbrotCanvas.addEventListener('click', function(event) {
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
   enterFullscreen()
-  updateHtml(zoom({factor: currentZoomFactor, xPosition:x, yPosition:y}, currentMandelbrot.parameter,currentMandelbrot.canvas))
-})
-
-canvas.mandelbrotCanvas.addEventListener('dblclick', function(event) {
-  const rect = canvas.mandelbrotCanvas.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
-  enterFullscreen()
-  updateHtml(zoom({factor: currentZoomFactor, xPosition:x, yPosition:y}, currentMandelbrot.parameter,currentMandelbrot.canvas))
+  const zoomFN = ()=>pipeline(()=>updateHtml(zoom({factor: currentZoomFactor, xPosition:x, yPosition:y}, currentMandelbrot.parameter,currentMandelbrot.canvas)))
+  if(currentZoomSteps == 1)
+  {
+  zoomFN()
+  return
+  }
+  zoomFN()
+  doXTimesEveryYms(()=>pipeline(()=>updateHtml(zoom({factor: currentZoomFactor, xPosition:500, yPosition:500}, currentMandelbrot.parameter,currentMandelbrot.canvas)))
+  ,currentZoomSteps-1, currentZoomDelay)
 })
 
 maxIterations?.addEventListener("keypress", (event: KeyboardEvent) => {
@@ -68,6 +80,22 @@ zoomFactor?.addEventListener("keypress", (event: KeyboardEvent) => {
   }
 })
 
+zoomSteps?.addEventListener("keypress", (event: KeyboardEvent) => {
+  if (event.key === "Enter") {
+    const target = event.target as HTMLInputElement;
+    if (!target || !target.value) return;
+    currentZoomSteps = (+target.value) 
+  }
+})
+
+zoomDelay?.addEventListener("keypress", (event: KeyboardEvent) => {
+  if (event.key === "Enter") {
+    const target = event.target as HTMLInputElement;
+    if (!target || !target.value) return;
+    currentZoomDelay = (+target.value) 
+  }
+})
+
 activateModuloColor?.addEventListener('change', function() {
   if (activateModuloColor && activateModuloColor.checked) {
     currentColorFunction = getColorFnWithBackground(getModuloColorCalculation(+red.value,+blue.value,+green.value))
@@ -81,7 +109,13 @@ activateModuloColor?.addEventListener('change', function() {
 
 canvas.mandelbrotCanvas?.addEventListener("contextmenu", (event: MouseEvent) => {
   event.preventDefault(); 
-  updateHtml(zoom({factor: 1/currentZoomFactor, xPosition:500, yPosition:500}, currentMandelbrot.parameter,currentMandelbrot.canvas))
+  const reversedZoomFactor = 1/currentZoomFactor
+  if(currentZoomSteps == 1)
+  {
+    pipeline(()=>updateHtml(zoom({factor: reversedZoomFactor, xPosition:500, yPosition:500}, currentMandelbrot.parameter,currentMandelbrot.canvas)))
+  }
+  doXTimesEveryYms(()=>pipeline(()=>updateHtml(zoom({factor: reversedZoomFactor , xPosition:500, yPosition:500}, currentMandelbrot.parameter,currentMandelbrot.canvas)))
+  ,currentZoomSteps-1, currentZoomDelay)
 })
 
 const handleKeyPress = (event: KeyboardEvent) => {
--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,10 @@ export function enterFullscreen() {
 export function delay(ms: number | undefined) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export async function doXTimesEveryYms(callback: ()=>any, Xtime: number, Ydelay: number, ) {
+  if(Xtime == 0)return
+  callback()
+  await delay(Ydelay)
+  doXTimesEveryYms(callback,Xtime-1,Ydelay)
+}
